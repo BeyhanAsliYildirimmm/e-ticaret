1688b8c6a785325e117fbc96c9833c903c21a337
Beyhan Aslı <yildirimbeyhan6@gmail.com>
2024-02-11 12:33:53 +0300

    Update UrunModal.php

--- modal/UrunModal.php
+++ modal/UrunModal.php
@@ -8,10 +8,6 @@ class UrunModal {
 
         $sorgu = $db->prepare("INSERT INTO products (product_name, detail, price, img_url) VALUES (?, ?, ?, ?)");
         $success = $sorgu->execute([$urunBilgisi['urunAdi'], $urunBilgisi['urunDetay'], $urunBilgisi['fiyat'], $urunBilgisi['urunResmi']]);
-       if($success)
-       {
-        echo "vöcxvö";
-       }
         return $success;
     }
 }
